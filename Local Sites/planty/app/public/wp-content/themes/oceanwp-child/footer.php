de2534096af6b9bea19bafc5bcfdd8ebfd548fe0
Cécile Andrisi <cecile.andrisi@gmail.com>
2025-02-13 14:12:06 +0100

    Ajouté Media Queries

---
+++ Local Sites/planty/app/public/wp-content/themes/oceanwp-child/footer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package OceanWP WordPress theme
+ */
+
+?>
+
+	</main><!-- #main -->
+
+<div class="centrer"><a id="footer" href="#"><?php echo"Mentions légales"; ?></a></div>	
+
+<?php wp_footer(); ?>
+</body>
+</html>
